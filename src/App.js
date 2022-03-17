e8a39b6085f1e2b556674907a8cae978287b11cc
Juan Antonio Sosa <juanan.sosa@gmail.com>
2022-03-17 12:52:33 +0100

    Changing App to a component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Movies from './Components/Movies';
 import Home from './Components/Home';
@@ -8,62 +8,82 @@ import OneMovie from './Components/OneMovie';
 import OneGenre from './Components/OneGenre';
 import EditMovie from './Components/EditMovie';
 
-export default function App() {
-  return ( 
-    <Router>
-      <div className='container'>
-        <div className='row'>
-          <h1 className='mt-3'>
-            Go Watch a Movie!
-          </h1>
-          <hr className='mb-3'></hr>
-        </div>
-        <div className='row'>
-          <div className='col-md-2'>
-            <nav>
-              <ul className="list-group">
-                <li className="list-group-item">                  
-                  <Link to="/">Home</Link>
-                </li>
-                <li className="list-group-item">                  
-                  <Link to="/movies">Movies</Link>
-                </li>
-                <li className="list-group-item">                  
-                  <Link to="/genres">Genres</Link>
-                </li>
-                <li className="list-group-item">
-                  <Link to="/admin/movie/0">Add Movie</Link>
-                </li>
-                <li className="list-group-item">
-                  <Link to="/admin">Manage Catalogue</Link>
-                </li>
-              </ul>
-            </nav>
+export default class App extends Component {
+
+  constructor(props){
+    super(props);
+    this.state = {
+      jwt: "",
+    }
+
+    this.handleJWTChange(this.handleJWTChange.bind(this));
+  }
+
+  handleJWTChange = (jwt) => {
+    this.setState({jwt: jwt});
+  }
+
+  logout = () => {
+    this.setState({jwt: ""});
+  }
+
+  render(){
+    return ( 
+      <Router>
+        <div className='container'>
+          <div className='row'>
+            <h1 className='mt-3'>
+              Go Watch a Movie!
+            </h1>
+            <hr className='mb-3'></hr>
           </div>
-          <div className='col-md-10'>
-            <Switch>
-              <Route path="/movies/:id" component={OneMovie}>                  
-              </Route>             
-              <Route path="/movies">
-                  <Movies />
-              </Route>
-              <Route exact path="/genres">
-                <Genres />
-              </Route> 
-              <Route path="/genre/:id" component={OneGenre}>                  
-              </Route>
-              <Route path="/admin/movie/:id" component={EditMovie}>                  
-              </Route>             
-              <Route path="/admin">
-                  <Admin />
-              </Route>             
-              <Route path="/">
-                  <Home />
-              </Route>
-            </Switch>
+          <div className='row'>
+            <div className='col-md-2'>
+              <nav>
+                <ul className="list-group">
+                  <li className="list-group-item">                  
+                    <Link to="/">Home</Link>
+                  </li>
+                  <li className="list-group-item">                  
+                    <Link to="/movies">Movies</Link>
+                  </li>
+                  <li className="list-group-item">                  
+                    <Link to="/genres">Genres</Link>
+                  </li>
+                  <li className="list-group-item">
+                    <Link to="/admin/movie/0">Add Movie</Link>
+                  </li>
+                  <li className="list-group-item">
+                    <Link to="/admin">Manage Catalogue</Link>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className='col-md-10'>
+              <Switch>
+                <Route path="/movies/:id" component={OneMovie}>                  
+                </Route>             
+                <Route path="/movies">
+                    <Movies />
+                </Route>
+                <Route exact path="/genres">
+                  <Genres />
+                </Route> 
+                <Route path="/genre/:id" component={OneGenre}>                  
+                </Route>
+                <Route path="/admin/movie/:id" component={EditMovie}>                  
+                </Route>             
+                <Route path="/admin">
+                    <Admin />
+                </Route>             
+                <Route path="/">
+                    <Home />
+                </Route>
+              </Switch>
+            </div>
           </div>
-        </div>
-      </div>   
-    </Router>
-  );
+        </div>   
+      </Router>
+    );
+  }
 }
